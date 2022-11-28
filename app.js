61b5e8b3be7c7f6ce978d7440596af5db60ecf4e
KoLario <felix.wong@gritacademy.se>
2022-11-28 17:17:22 +0100

    Added catch error function, one left

--- app.js
+++ app.js
@@ -8,11 +8,10 @@ DONE:
   - Befolkningsmängd
 * En png-bild med landets flagga
 * Länder från tidigare sök ska tas bort vid varje nytt sök. Får man 1 sökträff ska alltså endast ett land visas. Får man 20 sökträffar ska 20 länder visas. 
+* Om språket inte går att hitta ska ett informerande meddelande visas på webbsidan. Meddelandet ska tas bort när man söker på ett nytt land.
 --------------------------------------------------------
 LEFT TO DO:
 * Det landet med störst befolkningsmängd ska markeras på valfritt sätt. (Tex med text, färg eller border.)
-* Om språket inte går att hitta ska ett informerande meddelande visas på webbsidan. Meddelandet ska tas bort när man söker på ett nytt land.
-
 */
 
 const btn = document.querySelector("#country-button");
@@ -73,6 +72,9 @@ function displayCountry(langData) {
 });
 }
 
-function catchError(langData) {
-  // console.log(langData);
+function catchError() {
+  container.innerHTML = "";
+  const errorText = document.createElement('h1');
+  container.append(errorText);
+  errorText.innerText = 'No countries found, try again';
 }
